e37b7e4b28b9fa4b8715a953ed064eaf3f3a73da
SEB534542 <shj.vandermeulen@gmail.com>
2024-11-30 16:50:33 +0100

    NewWork added

--- gobookprices.go
+++ gobookprices.go
@@ -43,56 +43,8 @@ func findWork(isbn string) (string, error) {
 	return result.Works[0].Key, nil
 }
 
-// getEditions takes a reference to a work on openlibrary and the required languages.
-// It returns all IBNS13' that belong to that work. If no ISBN13 exists, it will return the ISBN10 for that edition.
-func getEditions(work string, langs []string) ([]string, error) {
-	url := fmt.Sprintf("https://openlibrary.org/%s/editions.json", work)
-	resp, err := http.Get(url)
-	if err != nil {
-		return []string{}, err
-	}
-	defer resp.Body.Close()
-
-	var result struct {
-		Entries []struct {
-			FullTitle string   `json:"full_title,omitempty"`
-			Isbn13    []string `json:"isbn_13"`
-			Isbn10    []string `json:"isbn_10"`
-			Languages []struct {
-				Key string `json:"key"`
-			} `json:"languages,omitempty"`
-			PhysicalFormat string   `json:"physical_format,omitempty"`
-			Series         []string `json:"series,omitempty"`
-		}
-	}
-
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return []string{}, err
-	}
-
-	// TODO: filter on languages and return only that list
-	editions := make([]string, 0, len(result.Entries))
-	for _, v := range result.Entries {
-		// check if edition is in requested language
-		for _, lang := range v.Languages {
-			if slices.Contains(langs, lang.Key) {
-				var isbns []string
-				switch {
-				case len(v.Isbn13) != 0:
-					isbns = append(isbns, v.Isbn13...)
-				case len(v.Isbn10) != 0:
-					isbns = append(isbns, v.Isbn10...)
-				}
-				editions = append(editions, isbns...)
-				break
-			}
-		}
-	}
-	return editions, nil
-}
-
-// GetWork takes an ISBN and preferred languages and returns the associated Work with underlying editions and an error.
-func GetWork(isbn string, languages []string) (Work, error) {
+// getWork takes an ISBN and preferred languages and returns the associated Work and an error.
+func getWork(isbn string, languages []string) (Work, error) {
 	work := Work{}
 	work.Languages = languages
 
@@ -138,7 +90,6 @@ func GetWork(isbn string, languages []string) (Work, error) {
 			work.Author += fmt.Sprintf(", %s", author)
 		}
 	}
-
 	return work, err
 }
 
@@ -162,6 +113,20 @@ func getAuthorDetails(author string) (string, error) {
 	return result.Name, nil
 }
 
+// NewWork takes an ISBN and preferred languages and returns the associated Work with underlying editions and an error.
+func NewWork(isbn string, languages []string) (Work, error) {
+	work, err := getWork(isbn, languages)
+	if err != nil {
+		return work, err
+	}
+	err = work.GetEditions()
+	if err != nil {
+		return work, err
+	}
+	return work, nil
+}
+
+// GetEditions gathers all editions beloning to the work from openlibary in the Work and returns an error.
 func (work *Work) GetEditions() error {
 	url := fmt.Sprintf("https://openlibrary.org/%s/editions.json", work.Url)
 	resp, err := http.Get(url)
@@ -187,7 +152,6 @@ func (work *Work) GetEditions() error {
 		return err
 	}
 
-	// TODO: filter on languages and return only that list
 	work.Editions = make([]Edition, 0, len(result.Entries))
 	for _, v := range result.Entries {
 		// check if edition is in requested language
--- gobookprices_test.go
+++ gobookprices_test.go
@@ -5,8 +5,7 @@ import (
 	"testing"
 )
 
-func TestGetWorks(t *testing.T) {
-	// TODO: test for editions with multiple works?
+func TestFindWork(t *testing.T) {
 	want := "/works/OL16807297W"
 	got, err := findWork("9780575104181")
 	switch {
@@ -17,31 +16,15 @@ func TestGetWorks(t *testing.T) {
 	}
 }
 
-func TestGetEditions(t *testing.T) {
-	// TODO: test with editions that have multiple ISBN?
-	t.Run("Get edition", func(t *testing.T) {
-		want := []string{
-			"9033405474",
-		}
-		got, err := getEditions("/works/OL5623531W", []string{"/languages/eng", "/languages/dut"})
-		switch {
-		case err != nil:
-			t.Errorf("Unknown error when fetching works for %v: %v", want, err)
-		case !reflect.DeepEqual(want, got):
-			t.Errorf("\nWant: '%+v', Got: '%+v'", want, got)
-		}
-	})
-
-	t.Run("Zero results test", func(t *testing.T) {
-		want := []string{} // no results expected
-		got, err := getEditions("/works/OL5623531W", []string{"/languages/eng"})
-		switch {
-		case err != nil:
-			t.Errorf("Unknown error when fetching works for %v: %v", want, err)
-		case !reflect.DeepEqual(want, got):
-			t.Errorf("\nWant: '%+v', Got: '%+v'", want, got)
-		}
-	})
+func TestGetAuthorDetails(t *testing.T) {
+	want := "Hendrik Cammaer"
+	got, err := getAuthorDetails("/authors/OL1357372A")
+	switch {
+	case err != nil:
+		t.Errorf("Unknown error when fetching works for %v: %v", want, err)
+	case want != got:
+		t.Errorf("\nWant: '%+v', Got: '%+v'", want, got)
+	}
 }
 
 func TestGetWork(t *testing.T) {
@@ -51,7 +34,7 @@ func TestGetWork(t *testing.T) {
 		Url:       "/works/OL5623531W",
 		Languages: []string{"/languages/eng", "/languages/dut"},
 	}
-	got, err := GetWork("9033405474", []string{"/languages/eng", "/languages/dut"})
+	got, err := getWork("9033405474", []string{"/languages/eng", "/languages/dut"})
 	switch {
 	case err != nil:
 		t.Errorf("Unknown error when fetching works for %v: %v", want, err)
@@ -75,13 +58,24 @@ func TestGetWork(t *testing.T) {
 	}
 }
 
-func TestGetAuthorDetails(t *testing.T) {
-	want := "Hendrik Cammaer"
-	got, err := getAuthorDetails("/authors/OL1357372A")
+func TestTemp(t *testing.T) {
+	want := Work{
+		Title:     "Menselijke levensloop",
+		Author:    "Hendrik Cammaer",
+		Url:       "/works/OL5623531W",
+		Languages: []string{"/languages/eng", "/languages/dut"},
+		Editions: []Edition{
+			{
+				Title:    "Menselijke levensloop",
+				Isbn:     "9033405474",
+				Language: "/languages/dut",
+			}},
+	}
+	got, err := NewWork("9033405474", []string{"/languages/eng", "/languages/dut"})
 	switch {
 	case err != nil:
 		t.Errorf("Unknown error when fetching works for %v: %v", want, err)
-	case want != got:
+	case !reflect.DeepEqual(want, got):
 		t.Errorf("\nWant: '%+v', Got: '%+v'", want, got)
 	}
 }
