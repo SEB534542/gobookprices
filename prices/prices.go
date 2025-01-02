23847f1d0e8a16f1484242ff94258ccef4e50f09
SEB534542 <shj.vandermeulen@gmail.com>
2025-01-02 11:19:31 +0100

    ob added

--- prices/prices.go
+++ prices/prices.go
@@ -2,14 +2,23 @@ package gobookprices
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const HostBol = "https://www.bol.com/nl/nl/s/"
+const (
+	HostBol  = "https://www.bol.com/nl/nl/s/"
+	HostKobo = "https://www.kobo.com/nl/en/"
+	HostOB   = "https://www.onlinebibliotheek.nl/"
+)
 
+var ErrorNotFound = fmt.Errorf("ISBN not found")
+
+// getPriceBol takes a URL to a Host and the isbn you want to get the price from Bol from. It returns the price and an error.
 func getPriceBol(hostUrl, isbn string) (float64, error) {
 	query := "?searchtext="
 	url := fmt.Sprint(hostUrl, query, isbn) // E.g. https://www.bol.com/nl/nl/s/?searchtext=9789021462691
@@ -38,8 +47,82 @@ func getPriceBol(hostUrl, isbn string) (float64, error) {
 			if err == nil {
 				price = parsedPrice
 			}
+		} else {
+			err = fmt.Errorf("failed to find price in html from '%s'", url)
 		}
 	})
 
 	return price, nil
+}
+
+func getPriceKobo(hostUrl, isbn string) (float64, error) {
+	query := "search?query="
+	url := fmt.Sprint(hostUrl, "/", query, isbn) // E.g. https://www.kobo.com/nl/en/search?query=9789021462691
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0.0, fmt.Errorf("getting price from %s failed: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return 0.0, fmt.Errorf("failed to load HTML from '%s': %w", url, err)
+	}
+
+	// Initialize a variable to store the price as float64
+	var price float64
+
+	// Find the span with class "price" and extract the text
+	doc.Find(".price-wrapper .price").Each(func(i int, s *goquery.Selection) {
+		priceText := strings.TrimSpace(s.Text()) // Extract the raw price text
+
+		// Remove the currency symbol (€) and replace the comma with a dot
+		cleanedPrice := strings.Replace(strings.TrimPrefix(priceText, "€ "), ",", ".", 1)
+
+		// Convert to float64
+		price, err = strconv.ParseFloat(cleanedPrice, 64)
+		if err != nil {
+			log.Printf("Error parsing price: %v", err)
+		} else {
+			fmt.Printf("Extracted price: %.2f\n", price) // Use the price as needed
+		}
+	})
+
+	return price, err
+}
+
+// getPriceOB takes a URL to a Host and the isbn you want to get the price from Bol from. It returns the price and an error.
+func getPriceOB(hostUrl, isbn string) (float64, error) {
+	query := "zoekresultaten.catalogus.html?q="
+	url := fmt.Sprint(hostUrl, query, isbn) // E.g. // https://www.onlinebibliotheek.nl/zoekresultaten.catalogus.html?q=9789021462691
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0.0, fmt.Errorf("getting price from %s failed: %w", url, err)
+	}
+	defer resp.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return 0.0, fmt.Errorf("failed to load HTML from '%s': %w", url, err)
+	}
+
+	// Check if the div with class 'content list-big' has any content
+	div := doc.Find("div.content.list-big")
+	if div.Length() == 0 {
+		// div not found
+		return 0.0, ErrorNotFound
+	}
+
+	// Check if it contains any child elements or text
+	content := strings.TrimSpace(div.Text())
+	if content == "" {
+		// div is empty
+		return 0.0, ErrorNotFound
+	}
+
+	return 0.0, nil
 }
--- prices/prices_test.go
+++ prices/prices_test.go
@@ -4,17 +4,27 @@ import (
 	_ "embed"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-//go:embed testdata/bol1.html
-var mockHtmlBol string
+var (
+	//go:embed testdata/bol1.html
+	mockHtmlBol string
+	//go:embed testdata/kobo1.html
+	mockHtmlKobo string
+	//go:embed testdata/ob1.html
+	mockHtmlOB1 string
+	//go:embed testdata/ob2.html
+	mockHtmlOB2 string
+)
 
-func TestGetPrice(t *testing.T) {
+func TestGetPriceBol(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(mockHtmlBol))
 	}))
+	defer server.Close()
 
 	isbn := "9789021462691"
 	want := 7.99
@@ -22,8 +32,71 @@ func TestGetPrice(t *testing.T) {
 
 	switch {
 	case err != nil:
-		t.Errorf("error getting price for '%s':Want: '%.2f'\tGot: '%.2f'\n", isbn, want, got)
+		t.Errorf("error getting price for '%s': %s\nWant: '%.2f'\tGot: '%.2f'\n", isbn, err, want, got)
 	case want != got:
 		t.Errorf("Want: '%.2f'\tGot: '%.2f'", want, got)
 	}
+}
+
+func TestGetPriceKobo(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(mockHtmlKobo))
+	}))
+	defer server.Close()
+
+	isbn := "9789021462691"
+	want := 7.99
+	// got, err := getPriceKobo(HostKobo, isbn)
+	got, err := getPriceKobo(server.URL, isbn)
+
+	switch {
+	case err != nil:
+		t.Errorf("error getting price for '%s': %s\nWant: '%.2f'\tGot: '%.2f'\n", isbn, err, want, got)
+	case want != got:
+		t.Errorf("Want: '%.2f'\tGot: '%.2f'", want, got)
+	}
+}
+
+func TestGetPriceOB(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.Contains(r.URL.RawQuery, "9789021462691") {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(mockHtmlOB1))
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(mockHtmlOB2))
+		}
+	}))
+	defer server.Close()
+
+	t.Run("default test", func(t *testing.T) {
+		isbn := "9789021462691"
+		want := 0.00
+		got, err := getPriceOB(HostOB, isbn)
+
+		switch {
+		case err != nil:
+			t.Errorf("error getting price for '%s': %s\nWant: '%.2f'\tGot: '%.2f'\n", isbn, err, want, got)
+		case want != got:
+			t.Errorf("Want: '%.2f'\tGot: '%.2f'", want, got)
+		}
+	})
+
+	t.Run("test with no results", func(t *testing.T) {
+		isbn := "97890214626918989"
+		want := 0.00
+		got, err := getPriceOB(HostOB, isbn)
+
+		if err != ErrorNotFound || want != got {
+			switch {
+			case err != ErrorNotFound:
+				t.Errorf("error getting price for '%s': %s\nWant: '%.2f'\tGot: '%.2f'\n", isbn, err, want, got)
+			case err != nil:
+				t.Errorf("error getting price for '%s': %s\nWant: '%.2f'\tGot: '%.2f'\n", isbn, err, want, got)
+			case want != got:
+				t.Errorf("Want: '%.2f'\tGot: '%.2f'", want, got)
+			}
+		}
+	})
 }
