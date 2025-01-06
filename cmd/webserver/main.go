4f1003fe3c3548e28bb9022011abf025d919581d
SEB534542 <shj.vandermeulen@gmail.com>
2025-01-06 09:54:50 +0100

    updated main to fetch all books and editions

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,42 +1,50 @@
 package main
 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"fmt"
-// 	"os"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 
-// 	goodreads "github.com/SEB534542/gobookprices/goodreads"
-// )
+	gr "github.com/SEB534542/gobookprices/goodreads"
+)
 
-// func main() {
-// 	url := "/review/list/68156753"
-// 	fmt.Println("Fetching books...")
-// 	books, err := goodreads.GetBooks(url, "to-read")
-// 	if err != nil {
-// 		fmt.Printf("error getting books from '%s': %v", url, err)
-// 	}
+func main() {
+	url := "/review/list/68156753"
+	shelf := "to-read"
+	log.Println("Creating libary...")
+	library := gr.NewLibrary(url, shelf, gr.EbookFormats, []string{"Dutch", "English"})
+	log.Println("Libary created")
+	log.Println("Fetching books...")
+	err := library.GetAll()
+	if err != nil {
+		log.Printf("error getting books from '%s': %v\n", url, err)
+		return
+	}
+	log.Println("Books fetched.")
 
-// 	fmt.Println("Saving to json...")
-// 	err = saveToJSON(books, "test.json")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
+	log.Println("Saving to json...")
+	err = saveToJSON(library, "library.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Println("Library saved to json.")
+}
 
-// // saveToJson takes an interface and stores it into the filename. It returns an error.
-// func saveToJSON(i interface{}, fileName string) error {
-// 	bs, err := json.Marshal(i)
-// 	if err != nil {
-// 		return fmt.Errorf("got error marshalling: %w\nfor file:\n%+V", err, i)
-// 	}
-// 	//  Use below if you want JSON pretty printed
-// 	var prettyJSON bytes.Buffer
-// 	_ = json.Indent(&prettyJSON, bs, "", "    ")
+// saveToJson takes an interface and stores it into the filename. It returns an error.
+func saveToJSON(i interface{}, fileName string) error {
+	bs, err := json.Marshal(i)
+	if err != nil {
+		return fmt.Errorf("got error marshalling: %w\nfor file:\n%+V", err, i)
+	}
+	//  Use below if you want JSON pretty printed
+	var prettyJSON bytes.Buffer
+	_ = json.Indent(&prettyJSON, bs, "", "    ")
 
-// 	err = os.WriteFile(fileName, prettyJSON.Bytes(), 0644)
-// 	if err != nil {
-// 		return fmt.Errorf("error saving JSON to '%s': %w", fileName, err)
-// 	}
-// 	return nil
-// }
+	err = os.WriteFile(fileName, prettyJSON.Bytes(), 0644)
+	if err != nil {
+		return fmt.Errorf("error saving JSON to '%s': %w", fileName, err)
+	}
+	return nil
+}
